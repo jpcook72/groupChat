802635e2f10456beed24d2431bd3d329e12b6180
John Cook <jpcook72@gmail.com>
2021-04-08 20:08:51 -0500

    Updating js files to TypeScript

--- server/index.tsx
+++ server/index.tsx
@@ -1,4 +1,6 @@
-const express = require('express')
+
+import express from 'express';
+
 const app = express()
 const path = require('path')
 
@@ -7,15 +9,19 @@ app.use(express.json())
 // serve static files
 app.use(express.static(path.join(__dirname, './public')))
 
+interface ResponseError extends Error {
+    status?: number;
+}
+
 // 404 handler
-app.use((req, res, next) => {
-    const error = Error(`Page not found(${req.url})`)
+app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+    const error: ResponseError = Error(`Page not found(${req.url})`)
     error.status = 404
     next(error)
 })
 
 // 500 handler
-app.use((err, req, res, next) => {
+app.use((err: ResponseError, req: express.Request, res: express.Response, next: express.NextFunction) => {
     res.status(err.status || 500).send(`
     <html>
       <body>
@@ -28,7 +34,7 @@ app.use((err, req, res, next) => {
 // listen on port
 const port = process.env.PORT || 3035
 
-const init = async () => {
+const init = async () : Promise<void> => {
     try {
         app.listen(port, () => console.log(`listening on port ${port}`))
     } catch (err) {
