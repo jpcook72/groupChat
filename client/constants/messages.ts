138af89f6b8a3a0efab070255bf8b3d7844d18da
John Cook <jpcook72@gmail.com>
2021-04-21 21:43:18 -0500

    TypeScript linting

--- client/constants/messages.ts
+++ client/constants/messages.ts
@@ -9,29 +9,29 @@ export const Ryan = {
 }
 
 export const fakeMessage = {
-	'sender': John,
-	'text': 'Hello, this is a test message',
-	'date': new Date(),
-	'likes': []
+	sender: John,
+	text: 'Hello, this is a test message',
+	date: new Date(),
+	likes: []
 }
 
 export const fakeMessages = [
 	{
-		'sender': John,
-		'text': 'Hello, what is going on',
-		'date': new Date(),
-		'likes': []
+		sender: John,
+		text: 'Hello, what is going on',
+		date: new Date(),
+		likes: []
 	},
 	{
-		'sender': Ryan,
-		'text': 'Hello, what is going on. I would really like to see what the long messages tend to look like',
-		'date': new Date(),
-		'likes': []
+		sender: Ryan,
+		text: 'Hello, what is going on. I would really like to see what the long messages tend to look like',
+		date: new Date(),
+		likes: []
 	},
 	{
-		'sender': John,
-		'text': 'Nothing much, what about you',
-		'date': new Date(),
-		'likes': []
+		sender: John,
+		text: 'Nothing much, what about you',
+		date: new Date(),
+		likes: []
 	}
 ]
